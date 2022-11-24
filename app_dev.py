51a19d0d4dfaeea2a30ba65b889c42c1a75e63bf
RobertoSanchezFormos <roberto.sanchez@formos.com>
2022-11-24 10:25:48 -0500

    AIRWEB-200: Fix Web API SQLite URI

--- app_dev.py
+++ app_dev.py
@@ -7,7 +7,7 @@ def run_development_api():
     host = "0.0.0.0"
     port = os.getenv("API_PORT", 8000)
     print(f">>>>> \tUI API deployed over: http://{host}:{port}/docs")
-    uvicorn.run("app.main:api", host=host, port=port, debug=True, reload=True)
+    uvicorn.run("app.main:api", host=host, port=port, reload=True)
 
 
 if __name__ == "__main__":
